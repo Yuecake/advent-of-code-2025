555b45cb8661f749e43d16be1871dea0c7a13333
Emma Haley <electric_guitar_@hotmail.com>
2026-09-24 23:22:09 +0100

    start day two

--- src/day_two/day_two.py
+++ src/day_two/day_two.py
@@ -9,9 +9,27 @@ def day_two_parse_instructions_file() -> int:
 
     instructions = "day_two/day_two_inputs.txt"
     
-    for value in read_file_comma_separated_values(instructions):
-        print(f"The current value is {value}!")
+    for product_id in read_file_comma_separated_values(instructions):
+        print(f"The current value is {product_id}!")
+        invalid_ids = find_invalid_ids(product_id)
+        print(invalid_ids)
 
+def find_invalid_ids(product_id) -> str:
+
+    invalid_ids = []
+
+    # Split the value by the -
+    for x in product_id.split('-'):
+        print(f"Examining {x} for repeating digits!")
+        for n in x:
+            if x.count(n) > 0:
+                print(f"{x} is a potential culprit!")
+                invalid_ids.append(x)
+
+    return invalid_ids
+
+def add_and_return_invalid_ids(id) -> int:
+    pass
 
 if __name__ == "__main__":
     day_two_parse_instructions_file()
